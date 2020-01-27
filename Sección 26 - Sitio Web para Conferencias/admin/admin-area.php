470d99d2f0a7b2a2367a9b87a6b07b8c9df21f5f
Bruno Francioni <47998211+BrunoFrancioni@users.noreply.github.com>
2020-01-27 20:52:55 -0300

    Create admin-area.php

---
+++ Sección 26 - Sitio Web para Conferencias/admin/admin-area.php
@@ -0,0 +1,47 @@
+<?php
+  include_once 'templates/header.php';
+  
+  include_once 'templates/barra.php';
+
+  include_once 'templates/navegacion.php';
+?>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="col-sm-6">
+          <h1>Blank Page</h1>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Title</h3>
+        </div>
+        <div class="card-body">
+          Start creating your amazing application!
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          Footer
+        </div>
+        <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+<?php
+  include_once 'templates/footer.php';
+?>
